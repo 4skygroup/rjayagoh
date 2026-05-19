425eee1ec9fda3c55f72f8a4b91ce07f72e51e9c
antoine-chen <antoinecoc2408@gmail.com>
2026-05-19 11:48:47 +0200

    debut vercel test2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
 import './css/Home.css';
 import './css/Style.css';
 import './css/Responsive.css';
-import Home from './pages/Home';
+import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import About from "./components/About.jsx";
+import Solutions from "./components/Solutions.jsx";
+import Frais from "./components/Frais.jsx";
+import Process from "./components/Process.jsx";
+import Avantages from "./components/Avantages.jsx";
+import Entreprises from "./components/Entreprises.jsx";
+import FAQ from "./components/Faq.jsx";
+import Contact from "./components/Contact.jsx";
+import Footer from "./components/Footer.jsx";
+import WhatsAppBtn from "./components/Whatsappbtn.jsx";
 
 export default function App() {
-  return <Home />;
+  return (
+      <>
+        <Navbar />
+        <Hero />
+        <About />
+        <Solutions />
+        <Frais />
+        <Process />
+        <Avantages />
+        <Entreprises />
+        <FAQ />
+        <Contact />
+        <Footer />
+        <WhatsAppBtn />
+      </>
+  );
 }
